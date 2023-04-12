ce9f6d1571bb2486d16933bbcc6ad2da0cae8f57
vadym.chornyy <vchornyi@intelliarts.com>
2023-04-12 12:49:18 +0300

    add test endpoint

--- src/main/java/com/example/brokergateway/controller/BinanceApiWebHooksListener.java
+++ src/main/java/com/example/brokergateway/controller/BinanceApiWebHooksListener.java
@@ -3,6 +3,7 @@ package com.example.brokergateway.controller;
 import com.example.brokergateway.dto.BaseOrderRequest;
 import com.example.brokergateway.service.LongPositionOrderProcessingService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class BinanceApiWebHooksListener {
     private final LongPositionOrderProcessingService service;
+
+    @GetMapping("/test")
+    public String test() {
+        return "Hello world!";
+    }
 
     @PostMapping("/binance/order")
     public void handleTradingViewAlert(@RequestBody BaseOrderRequest orderRequest) {
